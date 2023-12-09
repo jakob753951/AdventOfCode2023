9c69a4b39bc9eeb5250ccce09195a9809547dc8f
jakob753951 <jlm1999@live.dk>
2023-12-09 23:58:03 +0100

    added day_09

---
+++ day_09/part_1.py
@@ -0,0 +1,19 @@
+from itertools import pairwise
+
+with open('input.txt') as f:
+    lines = f.read().split('\n')
+
+lines = [list(map(int, line.split(' '))) for line in lines]
+
+def stuff(values: list[int]) -> int:
+    if all(value == 0 for value in values):
+        return 0
+    diffs = [b - a for a, b in pairwise(values)]
+
+    return values[-1] + stuff(diffs)
+
+
+results = [stuff(line) for line in lines]
+
+
+print(sum(results))
